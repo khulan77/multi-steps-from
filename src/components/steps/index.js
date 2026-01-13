661706f65ad567fff35d16bcb8e340b0b9c1c2f0
m2 <dhulan174@gmail.com>
2026-01-13 22:21:42 +0800

    nsdn

--- src/components/steps/index.js
+++ src/components/steps/index.js
@@ -2,7 +2,4 @@
  export * from "./PrivateInfo"
  export * from "./ProfileImage"
  export * from "./Success"
-// export { PrivateInfo } from "./PrivateInfo";
-// export { ContactInfo } from "./ContactInfo"; // ✅ яг файлтай таарах ёстой
-// export { ProfileImage } from "./ProfileImage";
-// export { Success } from "./Success";
+
