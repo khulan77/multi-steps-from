0f5153a49176b294d2686d9dca35afe64c2e1f2e
m2 <dhulan174@gmail.com>
2026-01-14 21:01:38 +0800

    sjbj

--- src/components/steps/Success.jsx
+++ src/components/steps/Success.jsx
@@ -1,25 +1,32 @@
 "use client";
 
 import { useEffect } from "react";
+import Image from "next/image";
 
 export const Success = () => {
   useEffect(() => {
-    // Success дээр ормогц хадгалалтыг цэвэрлэнэ
+    if (typeof window === "undefined") return;
+
     localStorage.removeItem("step");
     localStorage.removeItem("formValues");
   }, []);
 
   return (
-    <div className="flex h-48.25 flex-col items-start font-semibold justify-start p-7 gap-2">
+    <div className="flex h-48.25 flex-col font-semibold p-7 gap-2">
       <div className="w-15 h-15">
-        <img src="pinecone.svg.png" alt="" />
+        <Image
+          src="/pinecone.svg.png"
+          alt="success"
+          width={60}
+          height={60}
+        />
       </div>
-
-      <div className="text-[#202124] text-[26px] font-semibold">
+        
+      <div className="text-[#202124] text-[26px] flex font-semibold">
         You're All Set 🔥
       </div>
 
-      <div className="text-lg text-[#8e8e8e]">
+      <div className="text-lg  flex text-[#8e8e8e]">
         We have received your submission. Thank you!
       </div>
     </div>
